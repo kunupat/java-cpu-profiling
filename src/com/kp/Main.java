92434f060192f7a3e35036b0873e6b264d2910d8
Kunal Patil <kunal.m.patil@gmail.com>
2020-09-11 21:03:09 +0530

    Initial Commit

---
+++ src/com/kp/Main.java
@@ -0,0 +1,23 @@
+package com.kp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		timeConsumingMethod1();
+		System.out.println("Temp debug point");
+	}
+	
+	public static void timeConsumingMethod1() {
+		for(int i=0;i<5;i++)
+			timeConsumingMethod2();
+		
+		for(int i=0;i<1000000;i++)
+			System.out.println("Wasting Time by unnecessary work-1 :" + i+1);
+	
+	}
+	
+	public static void timeConsumingMethod2() {
+		for(int i=0;i<10000;i++)
+			System.out.println("Wasting Time by unnecessary work-2 :" + i+1);
+	}
+}
